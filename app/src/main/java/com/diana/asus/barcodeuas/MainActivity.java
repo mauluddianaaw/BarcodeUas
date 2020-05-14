f07e8646314c75025807009b6446abc71577c352
mauluddiana <mauluddiana@email.com>
2020-05-14 12:25:04 +0700

    layout,splash,login,register,main

---
+++ app/src/main/java/com/diana/asus/barcodeuas/MainActivity.java
@@ -0,0 +1,26 @@
+package com.diana.asus.barcodeuas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void Register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
